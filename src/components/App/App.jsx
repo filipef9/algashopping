d8e793b8126972ffc2eb00b70ef343b4416ce00d
filipef9 <filipef9@hotmail.com>
2022-03-21 22:18:50 +0000

    add linechart shared component.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import Checkbox from '../../shared/Checkbox'
+import LineChart from '../../shared/LineChart/LineChart'
 import AppContainer from '../AppContainer/AppContainer'
 import AppHeader from '../AppHeader'
 import { Container, Wrapper } from './App.styles'
 
 const App = () => {
   const [lettuce, setLettuce] = useState(true);
+  const [healthy, setHealthy] = useState(20);
+
+  const colors = ['#62cbc6', '#00abad', '#00b5bc', '#006073', '#004d61'];
 
   return (
     <Wrapper>
@@ -22,7 +26,30 @@ const App = () => {
             <div>
               sua lista de compras:
             </div>}
-          rightColumn={<div>estatísticas:</div>}
+          rightColumn={
+            <div>
+              estatísticas:
+              <LineChart 
+                title="Saudável"
+                percentage={80}
+                color={colors[0]}
+              />
+              <LineChart 
+                title="Não tão saudável assim"
+                percentage={20}
+                color={colors[1]}
+              />
+              <LineChart 
+                title="Limpeza"
+                percentage={35}
+                color={colors[2]}
+              />
+              <LineChart 
+                title="Outros"
+                percentage={15}
+                color={colors[3]}
+              />
+            </div>}
         />
       </Container>
     </Wrapper>
