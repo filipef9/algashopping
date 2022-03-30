9dd5357f66d8769b139b952baa1587e94b1d4840
filipef9 <filipef9@hotmail.com>
2022-03-30 13:53:24 +0000

    install basic redux.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import store from './store/store';
 import HomeView from './views/Home.view';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomeView />
+    <Provider store={store}>
+      <HomeView />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
